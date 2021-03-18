bcae1300ecf57b2c2b9b567e8309ae8297b994e4
Ryan Megarity <ryan_megarity@live.com>
2021-03-18 21:17:20 +0000

    added spotify logo

--- frontend/src/Components/Login.tsx
+++ frontend/src/Components/Login.tsx
@@ -4,15 +4,16 @@ import Container from "react-bootstrap/Container";
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
-export default function Login() {
+export const Login = () => {
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
-      style={{ minHeight: "100vh" }}
+      style={{ fontFamily: "Arial" }}
     >
       <a className="btn btn-success btn-lg" href={AUTH_URL}>
-        Login With Spotify
+        Login With Spotify{" "}
+        <img src="images/Spotify_Icon_RGB_White.png" alt="spotify icon" className="spotify-logo"></img>
       </a>
     </Container>
   );
-}
+};
--- frontend/src/Components/RoomCodeModal.tsx
+++ frontend/src/Components/RoomCodeModal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import { useHistory } from "react-router-dom";
-import Login from "./Login";
+import { Login } from "./Login";
 
 export const RoomCodeModal = (props: {
   show: any;
